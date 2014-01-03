54a9197f905af32fcc659b96969656b58262c677
kasperisager <kasperisager@gmail.com>
2014-01-03 16:11:30 -0500

    Comment out example vhosts as they may cause errors

--- puppet/manifests/provision.pp
+++ puppet/manifests/provision.pp
@@ -155,21 +155,21 @@ class provision::nginx::vhosts
 
   # Pre-defined setups
 
-  nginx::vhost { "vanilla":
-    root     => "${sites_dir}/vanilla",
-    index    => "index.php",
-    template => "${nginx_dir}/vanilla.conf.erb"
-  }
+  # nginx::vhost { "vanilla":
+  #   root     => "${sites_dir}/vanilla",
+  #   index    => "index.php",
+  #   template => "${nginx_dir}/vanilla.conf.erb"
+  # }
 
-  nginx::vhost { "kirby":
-    root     => "${sites_dir}/kirby",
-    index    => "index.php",
-    template => "${nginx_dir}/kirby.conf.erb"
-  }
+  # nginx::vhost { "kirby":
+  #   root     => "${sites_dir}/kirby",
+  #   index    => "index.php",
+  #   template => "${nginx_dir}/kirby.conf.erb"
+  # }
 
-  nginx::vhost { "statamic":
-    root     => "${sites_dir}/statamic",
-    index    => "index.php",
-    template => "${nginx_dir}/statamic.conf.erb"
-  }
+  # nginx::vhost { "statamic":
+  #   root     => "${sites_dir}/statamic",
+  #   index    => "index.php",
+  #   template => "${nginx_dir}/statamic.conf.erb"
+  # }
 }
