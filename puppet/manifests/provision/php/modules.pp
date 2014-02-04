b2e35cfd935b12ddc2b2ca346bf18a9b2c2152e9
Kasper Isager <kasperisager@gmail.com>
2014-02-04 17:57:08 -0500

    Update modules.pp

--- puppet/manifests/provision/php/modules.pp
+++ puppet/manifests/provision/php/modules.pp
@@ -14,14 +14,8 @@ class provision::php::modules
     notify => $notify_services
   }
 
-  php::module { [ "gd", "mysql", "sqlite", "mcrypt", "memcache", "xdebug" ]:
+  php::module { [ "gd", "mysql", "sqlite", "mcrypt", "memcache", "xdebug", "apcu" ]:
     notify  => $notify_services,
     content => $php_ini_dir
-  }
-
-  php::module { "apc":
-    package_prefix => "php-",
-    notify         => $notify_services,
-    content        => $php_ini_dir
   }
 }
