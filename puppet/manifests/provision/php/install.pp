135cbec4828696d5464073c7faf50affe1bbec07
kasperisager <kasperisager@gmail.com>
2014-05-27 20:19:52 -0400

    Remove old manifests and templates

--- puppet/manifests/provision/php/install.pp
+++
@@ -1,8 +0,0 @@
-# Class: provision::php::install
-#
-#
-class provision::php::install
-{
-  class { "php": }
-  class { "php::fpm": }
-}
--- puppet/manifests/provision/php/pools.pp
+++
@@ -1,9 +0,0 @@
-# Class: provision::php::pools
-#
-#
-class provision::php::pools
-{
-  php::fpm::pool { "www":
-    listen => $core::params::fpm_listen
-  }
-}
